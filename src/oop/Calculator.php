33d44e07a464862513d13d9b9a199bcbb24c0ab4
seoave <seo.ave@gmail.com>
2021-02-22 05:39:21 +0200

    oop ready

--- src/oop/Calculator.php
+++ src/oop/Calculator.php
@@ -126,7 +126,7 @@ class Calculator
      */
     public function undo()
     {
-        // TODO implement undo logic here
+        array_pop($this->intents);
 
         return $this;
     }
@@ -138,7 +138,8 @@ class Calculator
      */
     public function replay()
     {
-        // TODO implement replay logic here
+        $lastOperation = end($this->intents);
+        array_push($this->intents,$lastOperation);
 
         return $this;
     }
